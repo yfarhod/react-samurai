218118e9a98828839f8f2da1657b8bc0637b1609
yfarhod <yfarhod75@gmail.com>
2020-03-21 19:45:24 +0100

    lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="http://logok.org/wp-content/uploads/2014/05/Total-logo-earth-880x660.png"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" alt="" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
