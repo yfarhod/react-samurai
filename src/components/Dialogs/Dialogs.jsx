ebb209762a6a04cfdbf11cc9d367cffe0b80bc07
yfarhod <yfarhod75@gmail.com>
2020-03-23 21:52:55 +0100

    lesson 19

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+const Dialogs = () => {
+  return <div>Dialogs</div>;
+};
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" alt="" />
       </div>
