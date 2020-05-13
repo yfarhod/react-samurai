23b012ab7357858a7c1056f3c677e1e29e3f03da
yfarhod <yfarhod75@gmail.com>
2020-05-14 00:04:06 +0200

    lesson 61-1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className={s.header}>
       <img src="/images/avatar.png" alt="" />
+      <div className={s.itemBlock}>
+        <NavLink to="/login">login</NavLink>
+      </div>
     </header>
   );
 };
---
+++ src/redux/authReducer.js
@@ -0,0 +1,27 @@
+const SET_USER_DATA = 'SET_USER_DATA';
+
+const initialState = {
+  userId: null,
+  login: null,
+  email: null,
+  isAuth: false,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        ...action.data,
+      };
+    default:
+      return state;
+  }
+};
+
+export const setAuthUserData = (userId, login, email) => ({
+  type: SET_USER_DATA,
+  data: { userId, login, email },
+});
+
+export default authReducer;
